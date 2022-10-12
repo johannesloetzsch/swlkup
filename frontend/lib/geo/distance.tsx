3f410d5094794d050e253e552e6a33317a17b9d4
Johannes Lötzsch <git@johannesloetzsch.de>
2022-10-12 22:49:48 +0200

    fronted: fixed type

--- frontend/lib/geo/distance.tsx
+++ frontend/lib/geo/distance.tsx
@@ -2,7 +2,7 @@ export type lon=number
 export type lat=number
 
 /** optimization by https://stackoverflow.com/questions/5260423/torad-javascript-function-throwing-error/21623256#21623256 **/
-export function haversine_distance(lat1:lon, lon1: lon, lat2:lat, lon2:lon) {
+export function haversine_distance(lat1:lat, lon1: lon, lat2:lat, lon2:lon) {
   var R = 6371 // Radius of the earth in km
   var dLat = (lat2 - lat1) * Math.PI / 180  // deg2rad below
   var dLon = (lon2 - lon1) * Math.PI / 180
